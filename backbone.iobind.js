108750a73c55e5d5efeed1ed17011c9fb7f814da
Jake Luer <jake.luer@gmail.com>
2011-10-01 12:16:40 -0400

    made version 0.2.0

--- backbone.iobind.js
+++ backbone.iobind.js
@@ -5,8 +5,8 @@
  * https://logicalparadox.github.com/backbone.iobind
  */
  
-Backbone.Model.prototype.ioBindVersion = '0.1.2';
-Backbone.Collection.prototype.ioBindVersion = '0.1.2';
+Backbone.Model.prototype.ioBindVersion = '0.2.0';
+Backbone.Collection.prototype.ioBindVersion = '0.2.0';
 /*!
  * backbone.iobind - Model
  * Copyright(c) 2011 Jake Luer <@jakeluer>
