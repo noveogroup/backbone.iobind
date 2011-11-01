51161bdb023922f9f91a7d8afbd11f964a83c6be
Jake Luer <jake.luer@gmail.com>
2011-11-01 02:50:09 -0400

    starting sample express app

---
+++ example/app.js
@@ -0,0 +1,47 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , folio = require('folio')
+  , path = require('path');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', routes.index);
+
+var vendorJs = new folio.glossary([
+  require.resolve('jq/dist/jquery.js'),
+  require.resolve('underscore/underscore.js'),
+  require.resolve('backbone/backbone.js'),
+  path.join(__dirname, '..', 'backbone.iosync.js'),
+  path.join(__dirname, '..', 'backbone.iobind.js')
+]);
+
+app.get('/js/vendor.js', folio.serve(vendorJs));
+
+app.listen(1227);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
