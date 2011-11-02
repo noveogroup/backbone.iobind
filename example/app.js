e3d343725dcab1f02c50fd1f09c2574a91cf23af
Jake Luer <jake.luer@gmail.com>
2011-11-02 05:50:02 -0400

    app supports add and delete
    but its ugly :(

--- example/app.js
+++ example/app.js
@@ -4,13 +4,24 @@
  */
 
 var express = require('express')
+  , stylus = require('stylus')
+  , socketio = require('socket.io')
   , routes = require('./routes')
   , folio = require('folio')
-  , path = require('path');
+  , path = require('path')
+  , seed = require('seed');
 
 var app = module.exports = express.createServer();
 
 // Configuration
+
+var stylus_compile = function (str, path) {
+  return stylus(str)
+          .set('filename', path)
+          .set('compress', true)
+          .include(require('nib').path)
+          .include(require('fez').path);
+};
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
@@ -18,6 +29,12 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
+  app.use(stylus.middleware({
+    src: path.join(__dirname, 'styles'),
+    dest: path.join(__dirname, 'public'),
+    compile: stylus_compile,
+    force: true // this forces the css to be regenerated on every pageview
+  }));
   app.use(express.static(__dirname + '/public'));
 });
 
@@ -29,19 +46,88 @@ app.configure('production', function(){
   app.use(express.errorHandler()); 
 });
 
+// Our psuedo database
+
+var Minimal = {};
+
+Minimal.Comment = Seed.Model.extend('comment', {
+  schema: new Seed.Schema({
+    name: String,
+    body: String
+  })
+});
+
+Minimal.Todo = Seed.Model.extend('todo', {
+  schema: new Seed.Schema({
+    title: String,
+    completed: Boolean,
+    comments: [Minimal.Comment]
+  })
+});
+
+Minimal.Todos = Seed.Graph.extend({
+  initialize: function () {
+    this.define(Minimal.Todo);
+  }
+});
+
+var db = new Minimal.Todos()
+  , guid = new Seed.ObjectId();
+
+// Socket.io
+
+var io = require('socket.io').listen(app);
+
+io.sockets.on('connection', function (socket) {
+  
+  var create = function (data, callback) {
+    var id = guid.gen()
+      , todo = db.set('/todo/' + id, data)
+      , json = todo._attributes;
+    
+    socket.emit('todos:create', json);
+    socket.broadcast.emit('todos:create', json);
+    callback(null, json);
+  };
+  
+  socket.on('todo:create', create);
+  socket.on('todos:create', create);
+  
+  socket.on('todos:read', function (data, callback) {
+    var list = [];
+    
+    db.each('todo', function (todo) {
+      list.push(todo._attributes);
+    });
+    
+    callback(null, list);
+  });
+  
+  socket.on('todo:update', function (data, callback) {
+    var todo = db.get('/todo/' + data.id);
+    
+    todo.set(data);
+    
+    callback(null, todo);
+  });
+  
+  socket.on('todos:delete', function (data, callback) {
+    var json = db.get('/todo/' + data.id)._attributes;
+    
+    db.del('/todo/' + data.id);
+    
+    socket.emit('todos/' + data.id + ':delete', json);
+    socket.broadcast.emit('todos/' + data.id + ':delete', json);
+    callback(null, json);
+  });
+  
+});
+
 // Routes
 
 app.get('/', routes.index);
-
-var vendorJs = new folio.glossary([
-  require.resolve('jq/dist/jquery.js'),
-  require.resolve('underscore/underscore.js'),
-  require.resolve('backbone/backbone.js'),
-  path.join(__dirname, '..', 'backbone.iosync.js'),
-  path.join(__dirname, '..', 'backbone.iobind.js')
-]);
-
-app.get('/js/vendor.js', folio.serve(vendorJs));
+app.get('/js/templates.js', routes.templatejs);
+app.get('/js/vendor.js', routes.vendorjs);
 
 app.listen(1227);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
---
+++ example/public/js/example.js
@@ -0,0 +1,189 @@
+
+// usage: log('inside coolFunc', this, arguments);
+// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+window.log = function(){
+  log.history = log.history || [];   // store logs to an array for reference
+  log.history.push(arguments);
+  if(this.console) {
+    arguments.callee = arguments.callee.caller;
+    var newarr = [].slice.call(arguments);
+    (typeof console.log === 'object' ? log.apply.call(console.log, console, newarr) : console.log.apply(console, newarr));
+  }
+};
+
+window.socket = io.connect('http://localhost');
+
+log(window.socket);
+
+
+
+// We are going to call our app 'Minimal'.
+var Minimal = {};
+
+Minimal.App = Backbone.Router.extend({
+  routes: {
+    '': 'index',
+    '/': 'index'
+  },
+  index: function () {
+    var todos = new Minimal.Todos();
+    
+    var form = new Minimal.TodoListForm(todos);
+    $('#TodoWrapper').append(form.el);
+    
+    var list = new Minimal.TodoList(todos);
+    $('#TodoWrapper').append(list.el);
+    
+    todos.fetch();
+  }
+});
+
+Minimal.Todo = Backbone.Model.extend({
+  urlRoot: 'todo',
+  noIoBind: false,
+  initialize: function () {
+    _.bindAll(this, 'serverChange', 'serverDelete', 'modelCleanup');
+    
+    // if we are creating a new model to push to the server we don't want
+    // to iobind as we only bind new models from the server
+    if (!this.noIoBind) {
+      this.ioBind('update', window.socket, this.serverChange, this);
+      this.ioBind('delete', window.socket, this.serverDelete, this);
+    }
+  },
+  serverChange: function (data) {
+    // Useful to prevent loops when dealing with client-side updates (ie: forms).
+    data.fromServer = true;
+    this.set(data);
+  },
+  serverDelete: function (data) {
+    if (this.collection) {
+      this.collection.remove(this);
+    } else {
+      this.trigger('remove', this);
+    }
+    this.modelCleanup();
+  },
+  modelCleanup: function () {
+    this.ioUnbindAll(window.socket);
+    return this;
+  }
+});
+
+Minimal.Todos = Backbone.Collection.extend({
+  model: Minimal.Todo,
+  url: 'todos',
+  initialize: function () {
+    _.bindAll(this, 'serverCreate', 'collectionCleanup');
+    this.ioBind('create', window.socket, this.serverCreate, this);
+  },
+  serverCreate: function (data) {
+    // make sure no duplicates, just in case
+    var exists = this.get(data.id);
+    if (!exists) {
+      this.add(data);
+    } else {
+      data.fromServer = true;
+      exists.set(data);
+    }
+  },
+  collectionCleanup: function (callback) {
+    this.ioUnbindAll(window.socket);
+    this.each(function (model) {
+      model.modelCleanup();
+    });
+    return this;
+  }
+});
+
+Minimal.TodoList = Backbone.View.extend({
+  id: 'TodoList',
+  initialize: function(todos) {
+    _.bindAll(this, 'render', 'addTodo', 'removeTodo');
+    
+    this.todos = todos;
+    this.todos.bind('reset', this.render);
+    this.todos.bind('add', this.addTodo);
+    this.todos.bind('remove', this.removeTodo);
+    this.render();
+  },
+  render: function () {
+    var self = this;
+    
+    this.todos.each(function (todo) {
+      self.addTodo(todo);
+    });
+    
+    return this;
+  },
+  addTodo: function (todo) {
+    var tdv = new Minimal.TodoListItem(todo);
+    $(this.el).append(tdv.el);
+  },
+  removeTodo: function (todo) {
+    this.$('#' + todo.id).remove();
+  }
+});
+
+Minimal.TodoListItem = Backbone.View.extend({
+  className: 'todo',
+  events: {
+    'click .complete': 'completeTodo',
+    'click .delete': 'deleteTodo'
+  },
+  initialize: function (model) {
+    _.bindAll(this, 'completeTodo', 'deleteTodo');
+    this.model = model;
+    this.render();
+  },
+  render: function () {
+    $(this.el).html(template.item(this.model.toJSON()));
+    $(this.el).attr('id', this.model.id);
+    return this;
+  },
+  completeTodo: function () {
+    
+  },
+  deleteTodo: function () {
+    log('delete', this.model);
+    this.model.destroy({ silent: true });
+  }
+});
+
+Minimal.TodoListForm = Backbone.View.extend({
+  id: 'TodoForm',
+  events: {
+    'click .add': 'addTodo'
+  },
+  initialize: function (todos) {
+    _.bindAll(this, 'addTodo');
+    this.todos = todos;
+    this.render();
+  },
+  render: function () {
+    $(this.el).html(template.form());
+    return this;
+  },
+  addTodo: function () {
+    var Todo = Minimal.Todo.extend({
+      noIoBind: true
+    });
+    
+    var attrs = {
+      title: this.$('#TodoInput').val(),
+      completed: false
+    };
+    
+    this.$('#TodoInput').val('');
+    
+    var _todo = new Todo(attrs);
+    _todo.save();
+  }
+});
+
+$(document).ready(function () {
+  
+  window.app = new Minimal.App();
+  Backbone.history.start();
+  
+});
