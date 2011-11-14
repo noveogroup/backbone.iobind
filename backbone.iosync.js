7eedfd8c35c2047df49699743ea88b6c0b19bacc
Jake Luer <jake.luer@gmail.com>
2011-11-14 10:08:59 -0500

    build pull 3

--- backbone.iosync.js
+++ backbone.iosync.js
@@ -21,9 +21,10 @@
  * 
  * ### Assumptions
  * 
- * Currently expects active socket to be located at `window.socket` 
- * or `Backbone.socket`. See inline comments if you want to change it.
  * 
+ * Currently expects active socket to be located at `window.socket`,
+ * `Backbone.socket` or the sync'ed model own socket.
+ * See inline comments if you want to change it.
  * ### Server Side
  * 
  *     socket.on('todos:create', function (data, fn) {
@@ -51,7 +52,7 @@ Backbone.sync = function (method, model, options) {
   params.data = model.toJSON() || {};
   
   // If your socket.io connection exists on a different var, change here: 
-  var io = window.socket || Backbone.socket;
+  var io = model.socket || window.socket || Backbone.socket;
 
   io.emit(namespace + ':' + method, params.data, function (err, data) {
     if (err) {
