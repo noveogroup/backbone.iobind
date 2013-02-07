729011f7e5ac7e6caa6a003ed2ce9ccafb730b3c
Eugeny Vlasenko <mahnunchik@gmail.com>
2013-02-07 18:47:37 +0700

    fixed a compatibility with backbone 0.9.10

--- dist/backbone.iobind.js
+++ dist/backbone.iobind.js
@@ -2,11 +2,13 @@
   // Common JS // require JS
   var _, Backbone, exports;
   if (typeof window === 'undefined' || typeof require === 'function') {
+    $ = require('jquery');
     _ = require('underscore');
     Backbone = require('backbone');
     exports = Backbone;
     if (module) module.exports = exports;
   } else {
+    $ = this.$;
     _ = this._;
     Backbone = this.Backbone;
     exports = this;
