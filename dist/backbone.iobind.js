9f55583d832d448f08025bb57458e21df0502810
Matt Fletcher <naiveamoeba@gmail.com>
2015-06-04 11:54:57 +0100

    Added timeout option to sync methods.

--- dist/backbone.iobind.js
+++ dist/backbone.iobind.js
@@ -31,7 +31,7 @@
 /*!
  * Version
  */
-Backbone.Model.prototype.ioBindVersion = '0.4.8';
+Backbone.Model.prototype.ioBindVersion = '0.4.10';
 
 /**
  * # .ioBind(event, callback, [context])
@@ -178,7 +178,7 @@ Backbone.Model.prototype.ioUnbindAll = function (io) {
  * Version
  */
 
-Backbone.Collection.prototype.ioBindVersion = '0.4.8';
+Backbone.Collection.prototype.ioBindVersion = '0.4.10';
 
 /**
  * # ioBind
